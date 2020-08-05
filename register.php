27b2497f47c62e96420eaaf4fae90a75eff70ada
Daniel Rodriguez <drodriguez661@gmail.com>
2020-08-05 01:02:32 -0500

    asset folder, styles added and register page being worked at

--- register.php
+++ register.php
@@ -5,11 +5,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Welcome to movieflix!</title>
+  <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
 </head>
 
 <body>
-  hello
+
+  <div class="signInContainer">
+    <div class="column">
+
+    </div>
+  </div>
 
 </body>
 
